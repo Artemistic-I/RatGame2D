fdabdab9fb9c19e6e8a7560d52396d17232baf91
aidan-es <58824710+aidan-es@users.noreply.github.com>
2021-11-10 14:25:42 +0000

    Add timeline and tick method

--- Main.java
+++ Main.java
@@ -1,23 +1,37 @@
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
 
 	private static final int WINDOW_WIDTH = 1000;
 	private static final int WINDOW_HEIGHT = 1000;
+	
+	private Timeline tickTimeline; 
 
 	public void start(Stage primaryStage) throws Exception {
 		Pane root = buildGUI();
 		
 		Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 		
+		tickTimeline = new Timeline(new KeyFrame(Duration.millis(500), event -> tick()));
+		tickTimeline.setCycleCount(Animation.INDEFINITE);
+		tickTimeline.play();
+		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
+	private void tick() {
+		System.out.println("It's working...");
+	}
+
 	private Pane buildGUI() {
 		return new BorderPane();
 	}
