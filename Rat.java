8334e7ca4a16dac610ad06328ee8f661fbcba933
aidan-es <58824710+aidan-es@users.noreply.github.com>
2021-11-10 14:57:33 +0000

    Attributes and stub methods for Rat class

--- Rat.java
+++ Rat.java
@@ -1,8 +1,43 @@
 
 public class Rat {
 
-	public static void main(String[] args) {
+	private Boolean isMale;
+	private Boolean isAdult;
+	private Boolean isPregnant;
+	private Boolean isSterile;
+	private int speed;
+	private int colour;
+	private Tile tileTheRatIsOn;
 
+	public Tile move() {
+		return tileTheRatIsOn; //Just for testing
 	}
-
+	
+	public Boolean getIsMale() {
+		return isMale;
+	}
+	
+	public Boolean getIsAdult() {
+		return isAdult;
+	}
+	
+	public Boolean getIsPregnant() {
+		return isPregnant;
+	}
+	
+	public Boolean getIsSterile() {
+		return isSterile;
+	}
+	
+	public void breed() {
+		
+	}
+	
+	public void giveBirth() {
+		
+	}
+	
+	public void changeSex() {
+		
+	}
 }
