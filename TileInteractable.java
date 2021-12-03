3d730c94bd6c3bf01632aef3b17f8c93d7692174
Sam beard <sam210802@gmail.com>
2021-12-03 20:30:24 +0000

    Made a variable isInteractable so Stop Sign Item can change this for childs of TileInteractable Meaning that tile is not a possible move for rats

--- TileInteractable.java
+++ TileInteractable.java
@@ -4,33 +4,26 @@ import java.util.Random;
 import javafx.scene.image.Image;
 
 public class TileInteractable extends Tile {
-    // stores a list of rats currently on tile
-    // private ArrayList<Rat> ratsOnTile;
-    // // stores a list of items currently on tile
-    // private ArrayList<Item> itemOnTile;
 
     public TileInteractable(Image tileGraphic, int row, int column) {
     	super(tileGraphic, row, column);
+		this.setIsInteractable(true);
 	}
 
     public ArrayList<String> possibleMoves() {
         ArrayList <String> moves = new ArrayList<>();
-		if (this.getAdjacentTile("North") instanceof TileInteractable) {
+		if (this.getAdjacentTile("North").isInteractable()) {
 			moves.add("North");
 		}
-		if (this.getAdjacentTile("East") instanceof TileInteractable) {
+		if (this.getAdjacentTile("East").isInteractable()) {
 			moves.add("East");
 		}
-		if (this.getAdjacentTile("South") instanceof TileInteractable) {
+		if (this.getAdjacentTile("South").isInteractable()) {
 			moves.add("South");
 		}
-		if (this.getAdjacentTile("West") instanceof TileInteractable) {
+		if (this.getAdjacentTile("West").isInteractable()) {
 			moves.add("West");
 		}
 		return moves;
     }
-    
-    // isMating depends on if Tile stores the rats that are on it
-    // if so isMating can be set from inside the tile as it just has to check
-    // if there's two rats and if so are they of opposite gender and able to mate    
 }
