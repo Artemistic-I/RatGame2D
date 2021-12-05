459c55c7348dc2ca169616424aeef6c078077272
aidan-es <58824710+aidan-es@users.noreply.github.com>
2021-12-05 21:48:03 +0000

    remove uneeded

--- LethalItem.java
+++ LethalItem.java
@@ -1,23 +1,8 @@
 import javafx.scene.image.Image;
 
-//lethal items - kills rats
-// findRats method here as it appears in bomb and gas
-
 public abstract class LethalItem extends Item{
 
-    /**
-     * constructor method
-     */
     public LethalItem(Image itemGraphic, TileInteractable tileTheItemIsOn){
         super(itemGraphic, tileTheItemIsOn);
     }
-
-    /**
-     * method to kill rat and remove from board
-     * @param rat Rat to be removed
-     */
-    protected void killRat(Rat rat) {
-        RatManager.removeRat(rat);
-    }
-
 }
